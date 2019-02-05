8fcdfa5923cef8789b3c05468237450550720f9c
Lakhinana,Subodh Bhargav <s531388@ch140006m.nwmissouri.edu>
2019-02-05 12:50:08 -0600

    Initial Commit

---
+++ Time Converter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Time Converter
+//
+//  Created by Lakhinana,Subodh Bhargav on 2/5/19.
+//  Copyright © 2019 Lakhinana,Subodh Bhargav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
